6ee7711073021bda5924179effa86bb187786b05
Timothée Sergheraert <timothee.sergheraert.etu@univ-lille.fr>
2025-05-07 15:29:40 +0200

    Test JavaDOC

--- POO/src/basicclass/Student.java
+++ POO/src/basicclass/Student.java
@@ -1,5 +1,11 @@
 package basicclass;
 
+/*
+ * La classe représente un étudiant et comprend des méthodes permettant d'obtenir son âge, obtenir 
+ * @author <a>Clément Roty, Mano LEMAIRE, Timothée SERGHERAERT</a>
+ * @version 1.0
+ */
+
 import java.time.LocalDate;
 import java.util.HashMap;
 
@@ -53,15 +59,39 @@ public class Student{
         return this.constraintsMap;
     }
     @Override
+    /*
+     * Utilisée pour montrer les informations sur un étudiant
+     * @return Retourne une chaîne de caractères de la forme : Nom + Prénom
+     * @since 1.0
+     */
     public String toString(){
         return "Nom : "+this.name+" Prénom : "+this.forename;
     }
+    
+    /*
+     * Utilisée pour montrer toutes les informations possibles sur  un étudiant
+     * @return Retourne une chaîne de caractères plus complète que la fonction toString() de base, en utilisant la forme suivante : le ToString() de base + Sexe + Date de naissance + Pays + Liste des contraintes et préférences
+     * @since 1.0
+     */
     public String toStringComplete(){
         return "[ "+this.toString()+" Sexe : "+this.gender+" Date de naissance : "+this.birthday+" Pays : "+this.country+" ]\n-> Liste des contraintes / préférences : "+this.constraintsMap.toString();
     }
+    
+    /*
+     * Montre la liste des contraintes 
+     * @return Retourne la liste des contraintes et préférences en utilisant toString()
+     * @since 1.0
+     */
     public String viewConstraintsMap(){
         return "Liste des contraintes / préférences : "+this.constraintsMap.toString();
+
     }
+
+    /*
+     * Permet d'obtenir l'âge d'un étudiant grâce à un calcul.
+     * @return L'âge de l'étudiant
+     * @since 1.0
+     */
     public int getAge(){
         int age = LocalDate.now().getYear() - this.birthday.getYear();
         if(LocalDate.now().isBefore(this.birthday.withYear(LocalDate.now().getYear()))){
