728042eb5a97fc3f8a6401f56992bd279160ab6c
Clément Roty <clement.roty.etu@univ-lille.fr>
2025-05-07 15:37:58 +0200

    Début d'écriture des tests

---
+++ POO/test/basicclass/testAssociationStudent.java
@@ -0,0 +1,31 @@
+package basicclass;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class testAssociationStudent {
+    public Student s1,s2,s3,s4,s5,s6;
+    public AssociationStudent pair1,pair2,pair3;
+    public HashMap<Constraints,String> mapContraintes1,mapContraintes2,mapContraintes3;
+
+    @BeforeEach
+    public void initialization() {
+        this.s1=
+        this.s2=
+        this.s3=
+        this.s4=
+        this.s5=
+        this.s6=
+
+
+    }
+    
+}
